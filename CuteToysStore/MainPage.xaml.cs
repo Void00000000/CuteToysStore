3455db3692638a31e599c44a7fa9a8527f273daa
Void00000000 <chernikovearl@gmail.com>
2024-10-20 04:09:14 +0700

    Добавлена навигация по страницам

--- CuteToysStore/MainPage.xaml.cs
+++ CuteToysStore/MainPage.xaml.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Store;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -11,6 +13,7 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
@@ -25,6 +28,24 @@ namespace CuteToysStore
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private void NavigationView_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
+        {
+            NavigationViewItem item = args.InvokedItemContainer as NavigationViewItem;
+            if (item == null) return;
+            string clickedViewItem = item.Tag?.ToString();
+            var view = Assembly.GetExecutingAssembly().GetType($"CuteToysStore.{clickedViewItem}");
+            if (string.IsNullOrEmpty(clickedViewItem) || view == null) return;
+            ContentFrame.Navigate(view, null, new EntranceNavigationTransitionInfo());
+        }
+
+        private void NavigationView_Loaded(object sender, RoutedEventArgs e)
+        {
+            foreach (NavigationViewItemBase item in NavView.MenuItems)
+                if (item.Tag.ToString() == "StoreView")
+                    NavView.SelectedItem = item;
+            ContentFrame.Navigate(typeof(StoreView));
         }
     }
 }
